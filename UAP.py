ca0053b5d54d4242e9b0087922e27890ebe73949
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2022-12-12 12:25:18 +0700

    progress
    main jadi fungsi menu, soalnya yg pertama dipanggil login, dari login ntar manggil menu. ada yg disingkatin di menu ituu, jadi ga kepanjangan soalnya sintaksnya sama di 5 if elif, yang buat input qty dll. error handling sama else buat percabangan belum kelar. buat class lagi dibikin, isinya header doang sih, hiasan.

--- UAP.py
+++ UAP.py
@@ -44,76 +44,194 @@ def get_detail(index,data):
     print(f"[Nama Produk]\t: {data[index]['name']}")
     print(f"[Price]\t\t: Rp {data[index]['price']}\n")
     
+def menu() :
+    try:
+        pilih = int(input("Ibox Official Shop\n [1] iPhone \n [2] iPad\n [3] Apple Watch\n [4] Mac\n [5] Aksesoris Apple\n Pilih : "))
+        os.system('cls')
+        if pilih == 1 :
+            data = API_iBox.get_data(API_iBox.iphone)   
+
+        elif pilih == 2 : 
+            data = API_iBox.get_data(API_iBox.ipad)
+
+        elif pilih == 3 : 
+            data = API_iBox.get_data(API_iBox.iwatch)
+
+        elif pilih == 4 : 
+            data = API_iBox.get_data(API_iBox.mac)
+
+        elif pilih == 5 : 
+            data = API_iBox.get_data(API_iBox.aksesoris)
+
+        else:
+            print("Kategori Tidak Ada!")  
+            quit()   
+
+        product_list(data) 
+        index = int(input("\nPilih : "))
+        get_detail(index,data)
+        qty = int(input('Jumlah Beli : '))  
+        if qty > data[index]['stock'] :
+            print(f"Stok Tidak Cukup!\n[Stock]\t\t: {data[index]['stock']}")
+            menu()
+        elif qty <= 0 :
+            print("Jumlah beli tidak dapat diproses.")
+            menu()
+        payment(index,qty,data)
+                                                          
+    except ValueError :
+        print("Inputan Salah!")   
+
+def login() :
+    global user, unem, alamat
+    os.system('cls')
+    # hias.shopee()
+    print('[1] LOG IN')
+    print('[2] SIGN UP')
+    akun = int(input('\nMasuk dengan : '))
+    if akun == 1 :
+        unem = input('Username\t: ')
+        pw = input('Password\t: ')
+        for cek in user :
+            if cek['username'] == unem  and cek['password'] == pw :
+                alamat = cek['alamat']
+                os.system('cls')
+                menu()   
+                quit()           
+        else :
+            print('Harap Masukkan Username atau Password yang terdaftar!')
+            time.sleep(2)
+            login()
+    elif akun == 2 :
+        unem = input('Username\t: ')
+        pw = input('Password\t: ')
+        for cek in user :
+            if cek['username'] == unem :
+                print('Username sudah digunakan!')
+                time.sleep(2)
+                login()
+        else :
+            new['username'] = unem
+            new['password'] = pw
+            user.append(new)
+            print('Akun tersimpan!\n Silahkan login')
+            time.sleep(2)
+            login()
+    else :
+        print("Harap Masuk dengan 1 atau 2.")
+        time.sleep(2)
+        login()
+        
 def payment(index,qty,data):
+    global unem, alamat
     index -= 1
     harga = int(data[index]['price']*qty) 
     print(f'\nQty : {qty}')
     print(f'Total Harga : Rp {harga}')
+    if alamat == '' :
+        alamat = input('Masukkan Alamat : ') 
+    else :
+        print(f'Alamat Pengiriman : {alamat}')
+        alamat = ''
+        ubah = input("Ubah Alamat? (y/t) ")
+        change = ubah.lower()
+        if change == 'y' :
+            alamat = input('Masukkan Alamat : ')
+    pil = input("\nCheckout? (y/t) ")
+    co = pil.upper()
+    print(co)
+    if co == 'Y' :
+        data[index]['stock'] = 0
+        print(f'Nama Akun : {unem}\n')
+        print(f"Nama Barang : {data[index]['name']}")
+        print(f"Harga Barang : {data[index]['price']}")
+        print(f'Qty : {qty}')
+        print(f'Total Harga : Rp {harga}\n')
+        print(f'Alamat Pengiriman : {alamat}')
+
+        bukti = input("Apakah anda ingin mengunduh bukti transaksi?")
+        unduh = bukti.upper()
+        
+        if unduh == 'Y' :
+            file = open("Shopee Transaction.txt","w")
+            file.write("SHOPEE CHECKOUT\n")
+
+            file.write(f'Nama Akun : {unem}\n\n\n')
+            file.write(f"Nama Barang : {data[index]['name']}\n")
+            file.write(f"Harga Barang : {data[index]['price']}\n")
+            file.write(f'Qty : {qty}\n')
+            file.write(f'Total Harga : Rp {harga}\n\n')
+            file.write(f'Alamat Pengiriman : {alamat}')
+            file.close()
+            print("File Sudah Diunduh")
+            print()
+   
+login()
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    try:
-        pilih = int(input("Ibox Official Shop\n 1. iPhone \n 2. iPad\n 3. Apple Watch\n 4. Mac\n 5. Aksesoris Apple\n Pilih : "))
-        if pilih == 1 :
-            data = get_data(iphone)
-            product_list(data)
-            index = int(input("\nPilih : "))
-            try:
-                get_detail(index,data)
-                qty = int(input('Jumlah Beli : '))
-                payment(index,qty,data)
-            except:
-                print("Barang tidak ada!")    
+#     try:
+#         pilih = int(input("Ibox Official Shop\n 1. iPhone \n 2. iPad\n 3. Apple Watch\n 4. Mac\n 5. Aksesoris Apple\n Pilih : "))
+#         if pilih == 1 :
+#             data = get_data(iphone)
+#             product_list(data)
+#             index = int(input("\nPilih : "))
+#             try:
+#                 get_detail(index,data)
+#                 qty = int(input('Jumlah Beli : '))
+#                 payment(index,qty,data)
+#             except:
+#                 print("Barang tidak ada!")    
 
-        elif pilih == 2 : 
-            data = get_data(ipad)
-            product_list(data) 
-            index = int(input("\nPilih : "))
-            try:
-                get_detail(index,data)
-                qty = int(input('Jumlah Beli : '))
-                payment(index,qty,data)
-            except:
-                print("Barang tidak ada!")
+#         elif pilih == 2 : 
+#             data = get_data(ipad)
+#             product_list(data) 
+#             index = int(input("\nPilih : "))
+#             try:
+#                 get_detail(index,data)
+#                 qty = int(input('Jumlah Beli : '))
+#                 payment(index,qty,data)
+#             except:
+#                 print("Barang tidak ada!")
 
-        elif pilih == 3 : 
-            data = get_data(iwatch)
-            product_list(data) 
-            index = int(input("\nPilih : "))
-            try:
-                get_detail(index,data)
-                qty = int(input('Jumlah Beli : '))
-                payment(index,qty,data)
-            except:
-                print("Barang tidak ada!")   
+#         elif pilih == 3 : 
+#             data = get_data(iwatch)
+#             product_list(data) 
+#             index = int(input("\nPilih : "))
+#             try:
+#                 get_detail(index,data)
+#                 qty = int(input('Jumlah Beli : '))
+#                 payment(index,qty,data)
+#             except:
+#                 print("Barang tidak ada!")   
 
-        elif pilih == 4 : 
-            data = get_data(mac)
-            product_list(data) 
-            index = int(input("\nPilih : "))
-            try:
-                get_detail(index,data)
-                qty = int(input('Jumlah Beli : '))
-                payment(index,qty,data)
-            except:
-                print("Barang tidak ada!")
+#         elif pilih == 4 : 
+#             data = get_data(mac)
+#             product_list(data) 
+#             index = int(input("\nPilih : "))
+#             try:
+#                 get_detail(index,data)
+#                 qty = int(input('Jumlah Beli : '))
+#                 payment(index,qty,data)
+#             except:
+#                 print("Barang tidak ada!")
 
-        elif pilih == 5 : 
-            data = get_data(aksesoris)
-            product_list(data) 
-            index = int(input("\nPilih : "))
-            try:
-                get_detail(index,data)
-                qty = int(input('Jumlah Beli : '))
-                payment(index,qty,data)
-            except:
-                print("Barang tidak ada!")   
+#         elif pilih == 5 : 
+#             data = get_data(aksesoris)
+#             product_list(data) 
+#             index = int(input("\nPilih : "))
+#             try:
+#                 get_detail(index,data)
+#                 qty = int(input('Jumlah Beli : '))
+#                 payment(index,qty,data)
+#             except:
+#                 print("Barang tidak ada!")   
 
-        else:
-            print("Kategori Tidak Ada!")                                                  
-    except:
-        print("Inputan Salah!")   
+#         else:
+#             print("Kategori Tidak Ada!")                                                  
+#     except:
+#         print("Inputan Salah!")   
         
         
         
